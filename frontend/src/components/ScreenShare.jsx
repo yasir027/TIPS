86648cd1264688c8783f361637b5a752be46a178
yasir027 <116998637+yasir027@users.noreply.github.com>
2025-01-23 14:57:04 +0530

    Update ScreenShare.jsx

--- frontend/src/components/ScreenShare.jsx
+++ frontend/src/components/ScreenShare.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://192.168.127.117:3000'); // Replace with your backend URL
+const socket = io('http://192.168.1.16:3000'); // Replace with your backend URL
 
 const ScreenShare = () => {
     useEffect(() => {
